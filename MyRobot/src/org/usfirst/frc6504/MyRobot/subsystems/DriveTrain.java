a72ccf91f152c20676d3c3610d3aa8559625a30c
Chandi Kanhai <chandikanhai@gmail.com>
2018-02-17 10:33:44 -0500

    Map xbox triggers to z axis

--- MyRobot/src/org/usfirst/frc6504/MyRobot/subsystems/DriveTrain.java
+++ MyRobot/src/org/usfirst/frc6504/MyRobot/subsystems/DriveTrain.java
@@ -66,8 +66,9 @@ public class DriveTrain extends Subsystem {
     
     //Taking Joystick input/getting input from gyro
     public void takeJoystickInput(Joystick logitechJoystick0) {
+    	double z = logitechJoystick0.getRawAxis(2) - logitechJoystick0.getRawAxis(3);
     	robotDrive.driveCartesian(logitechJoystick0.getX(), -logitechJoystick0.getY(),
-    			logitechJoystick0.getZ(), RobotMap.gyro.getAngle());
+    			z, RobotMap.gyro.getAngle());
     	//robotDrive.driveCartesian(-logitechJoystick0.getX(), -logitechJoystick0.getY(),
     	//		-logitechJoystick0.getZ(), RobotMap.gyro.getAngle());
     	//robotDrive.driveCartesian(xbox360joyStick1.getRawAxis(0), xbox360joyStick1.getRawAxis(0), 0.0);
