4b4724967045506b65b5614d80bcd3f69e33c250
Chandi Kanhai <chandikanhai@gmail.com>
2018-03-11 15:49:52 -0400

    Final Competition Code
    Final touches to the competition code.

--- MyRobot/src/org/usfirst/frc6504/MyRobot/commands/ClimberUp.java
+++ MyRobot/src/org/usfirst/frc6504/MyRobot/commands/ClimberUp.java
@@ -44,8 +44,8 @@ public class ClimberUp extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-    	RobotMap.climbingSubsystemClimber1.set(-1);
-    	RobotMap.climbingSubsystemClimber2.set(-1);
+    	RobotMap.climbingSubsystemClimber1.set(.6);
+    	RobotMap.climbingSubsystemClimber2.set(.6);
     }
 
     // Make this return true when this Command no longer needs to run execute()
